901284126d874cdb823d719946d962807fe88f5f
Sofia Ochkalova <so.ochkalova@gmail.com>
2026-01-08 12:02:53 +0000

    linting

--- bin/download_fasta_utils.py
+++ bin/download_fasta_utils.py
@@ -139,9 +139,8 @@ def load_genbank_locations(gca_accessions: set) -> dict[str, str | None]:
             if not line or line.startswith("#"):
                 continue
             fields = line.rstrip("\n").split("\t")
-            accession = fields[0].split(".")[
-                0
-            ]  # accession in the table always has version, remove it
+            # accession in the table always has version, remove it
+            accession = fields[0].split(".")[0]
             if accession in locations:
                 ftp_path = fields[19]
                 if ftp_path != "na":
